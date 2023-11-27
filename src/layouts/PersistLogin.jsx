e4a638385e71a0304d9dc0da9fb024d4beae4187
joelricmisa <joelriccmisa@gmail.com>
2023-11-27 14:47:59 +0800

    added persist login feature

---
+++ src/layouts/PersistLogin.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () => {
+    const refresh = useRefreshToken();
+    const { auth } = useAuth();
+
+    useEffect(() => {
+        const verifyRefreshToken = async () => {
+            try {
+                await refresh();
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        !auth?.accessToken ? verifyRefreshToken() : null;
+    }, []);
+
+    return <></>;
+};
+
+export default PersistLogin;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,16 +3,18 @@ import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import { ScrollToTop } from "../components";
+import PersistLogin from "./PersistLogin";
 
 const RootLayout = () => {
-  return (
-    <div className="relative overflow-clip font-poppins text-base">
-      <ScrollToTop />
-      <Navbar />
-      <main>{<Outlet />}</main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="relative overflow-clip font-poppins text-base">
+            <ScrollToTop />
+            <PersistLogin />
+            <Navbar />
+            <main>{<Outlet />}</main>
+            <Footer />
+        </div>
+    );
 };
 
 export default RootLayout;
