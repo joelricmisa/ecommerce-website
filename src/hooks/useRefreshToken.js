8ee01a3689d90993bd6fac740ac8d3252a28cded
joelricmisa <joelriccmisa@gmail.com>
2023-12-01 17:07:42 +0800

    comment console

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -10,7 +10,7 @@ const useRefreshToken = () => {
         });
 
         setAuth((prev) => {
-            console.log(response?.data?.accessToken);
+            // console.log(response?.data?.accessToken);
             return {
                 ...prev,
                 role: response?.data?.role,
