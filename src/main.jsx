c5b050c9d15cf88edaf7a614daefa9a9a56047c0
joelricmisa <joelriccmisa@gmail.com>
2023-12-04 22:04:25 +0800

    chore(main.js): modify default options for refetching in the queryclient

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,15 @@ import "./index.css";
 import { AuthProvider } from "./contexts/AuthProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 2,
+            retryDelay: 5000,
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
