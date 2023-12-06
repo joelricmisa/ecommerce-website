bcd348b7ab104cfd2829b7ee4410aee5888c9678
joelricmisa <joelriccmisa@gmail.com>
2023-12-06 19:46:34 +0800

    refactor(main.js): change refetch to 5 mins staletime

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            refetchOnWindowFocus: false,
+            staleTime: 1000 * 60 * 5,
             retry: 2,
             retryDelay: 5000,
         },
