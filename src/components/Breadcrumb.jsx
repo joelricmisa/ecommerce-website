32d1986701de6c310f5cf03ae778ba8d29b5494a
joelricmisa <joelriccmisa@gmail.com>
2023-12-07 19:22:47 +0800

    refactor: Breadcrumb component

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,30 +1,28 @@
 import { useLocation, Link } from "react-router-dom";
 
 const Breadcrumb = ({ padding = "padding-y" }) => {
-  const location = useLocation();
-  let currentLink = "";
-  const crumbs = location.pathname.split("/").filter((link) => link !== "");
+    let currentLink = "";
+    const location = useLocation();
+    const crumbs = location.pathname.split("/").filter((link) => link !== "");
 
-  return (
-    <p className={`${padding}`}>
-      <Link to="/" className="opacity-50 ">
-        Home /
-      </Link>
+    const linkStyle = `ml-2 capitalize opacity-50 after:ml-2 after:content-['/'] last:opacity-100 after:last:content-['']`;
 
-      {crumbs.map((link, index) => {
-        currentLink += `/${link}`;
-        return (
-          <Link
-            to={currentLink}
-            className="ml-2 capitalize opacity-50 after:ml-2 after:content-['/'] last:opacity-100 after:last:content-['']"
-            key={index}
-          >
-            {link}
-          </Link>
-        );
-      })}
-    </p>
-  );
+    return (
+        <p className={`${padding}`}>
+            <Link to="/" className="opacity-50 ">
+                Home /
+            </Link>
+
+            {crumbs.map((link, index) => {
+                currentLink += `/${link}`;
+                return (
+                    <Link to={currentLink} className={linkStyle} key={index}>
+                        {link}
+                    </Link>
+                );
+            })}
+        </p>
+    );
 };
 
 export default Breadcrumb;
