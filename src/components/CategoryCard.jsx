dde4abbe4985fcca732322f99affa0c5b53c685d
joelricmisa <joelriccmisa@gmail.com>
2023-12-07 19:23:11 +0800

    refactor: category card component

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -4,10 +4,12 @@ import { ShopContext } from "../contexts/ShopContext";
 
 const CategoryCard = ({ categoryName, categoryImage }) => {
     const { setCategory } = useContext(ShopContext);
+    const linkStyle = `cursor-pointer rounded-sm border-2 border-black/30 py-7 transition-transform hover:scale-105 hover:bg-extraColor hover:font-medium`;
+
     return (
         <Link
             to={"/products"}
-            className="cursor-pointer rounded-sm border-2 border-black/30 py-7 transition-transform hover:scale-105 hover:bg-extraColor hover:font-medium"
+            className={linkStyle}
             onClick={() => setCategory(categoryName)}
         >
             <span className="grid-center gap-5">
