d0dc1334c801d745438fc99a813f51e08598bf69
joelricmisa <joelriccmisa@gmail.com>
2023-12-07 19:27:32 +0800

    fix(logout): add queryclient.clear in logout hook to fix the bug

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import useAxiosPrivate from "./useAxiosPrivate";
 import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
+import { ShopContext } from "../contexts/ShopContext";
+import { useQueryClient } from "@tanstack/react-query";
 
 const useLogout = () => {
     const axiosPrivate = useAxiosPrivate();
-    const navigate = useNavigate();
     const { setAuth } = useAuth();
+    const { setCartItems, setWishlistItems } = useContext(ShopContext);
+
     const LOGOUT_URL = "/api/logout";
+    const navigate = useNavigate();
+
+    const queryClient = useQueryClient();
 
     const handleLogout = async () => {
         try {
@@ -18,7 +24,12 @@ const useLogout = () => {
             });
 
             console.log(response?.data);
+
             setAuth(null);
+            setCartItems([]);
+            setWishlistItems([]);
+            queryClient.clear();
+
             navigate("/", { replace: true });
         } catch (err) {
             console.log(err);
