93c9a225fa2a5f7decb76bd1d361dcfdde534c76
joelricmisa <joelriccmisa@gmail.com>
2023-12-09 21:55:49 +0800

    refactor(Cart Card)

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,45 +1,47 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../contexts/ShopContext";
 import { FaXmark } from "react-icons/fa6";
-const CartCard = ({ _id, name, image, price, quantity }) => {
+const CartCard = ({ _id, name, image, price, quantity, discount }) => {
     const { cartItems, setCartItems, removeCartItem } = useContext(ShopContext);
-    const [state, setState] = useState(quantity);
-    const [productSubTotal, setProductSubTotal] = useState(
-        Number(price) * Number(state),
-    );
+    const [qty, setQty] = useState(quantity);
+    const finalPrice = Number(price) - Number(price) * (discount / 100);
+    const productSubTotal = Number(finalPrice) * Number(qty);
+
     const formatNumber = new Intl.NumberFormat("fil-PH", {
         currency: "PHP",
         style: "currency",
     });
     const baseUrl = "https://exclusive-backend-te81.onrender.com";
 
-    useEffect(() => {
-        setProductSubTotal(Number(price) * Number(state));
+    const imageSource = ` ${baseUrl}${image
+        ?.replace("public", "")
+        ?.replaceAll("\\", "/")}`;
 
+    useEffect(() => {
         const cartList = cartItems.map((item) =>
             item._id === _id
                 ? {
                       ...item,
-                      quantity: state,
-                      subTotal: productSubTotal,
+                      quantity: qty,
                   }
                 : item,
         );
+        //not auth
         setCartItems(cartList);
-    }, [state, productSubTotal]);
+        //auth
+        // updateCartItems();
+    }, [qty]);
 
+    //not auth
     useEffect(() => {
-        setState(quantity);
+        setQty(quantity);
     }, [removeCartItem]);
 
     return (
         <div className="flex-center relative w-full gap-0 rounded-sm shadow-sm last:mb-0">
             <div className="flex-center w-1/2 flex-col justify-evenly border-r border-black/10 px-2 py-5 xl:w-2/6 xl:flex-row xl:justify-start xl:border-r-0 xl:pl-28 ">
                 <img
-                    src={
-                        baseUrl +
-                        image?.replace("public", "")?.replaceAll("\\", "/")
-                    }
+                    src={imageSource}
                     className="h-16 w-20 object-contain"
                     alt=""
                 />
@@ -47,14 +49,15 @@ const CartCard = ({ _id, name, image, price, quantity }) => {
             </div>
             <div className="flex-center w-1/2 flex-col pb-10 pt-14 xl:w-4/6 xl:flex-row">
                 <div className="flex-center justify-evenly xl:w-1/2 ">
-                    <p>{formatNumber.format(price)} (1)</p>
+                    <p>{formatNumber.format(finalPrice)} (1)</p>
 
                     <input
                         type="number"
                         min={1}
-                        value={state < 10 ? `0${state}` : state}
+                        max={99}
+                        value={quantity < 10 ? `0${quantity}` : quantity}
                         className="input w-16 text-center font-inter "
-                        onChange={(e) => setState(e.target.value)}
+                        onChange={(e) => setQty(e.target.value)}
                     />
                 </div>
                 <div className="flex-center justify-evenly xl:w-1/2 ">
