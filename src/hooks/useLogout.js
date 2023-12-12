7721b99248555bc4b54a99e636e998e1a7d12549
joelricmisa <joelriccmisa@gmail.com>
2023-12-12 21:25:55 +0800

    fix(logout): undefined output for alert

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -4,18 +4,20 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
 import { ShopContext } from "../contexts/ShopContext";
 import { useQueryClient } from "@tanstack/react-query";
+import FeedbackContext from "../contexts/FeedbackProvider";
 
 const useLogout = () => {
     const axiosPrivate = useAxiosPrivate();
     const { setAuth } = useAuth();
     const { setCartItems, setWishlistItems } = useContext(ShopContext);
+    const { setType, setShowAlert, setMessage } = useContext(FeedbackContext);
 
     const LOGOUT_URL = "/api/logout";
     const navigate = useNavigate();
 
     const queryClient = useQueryClient();
 
-    const handleLogout = async () => {
+    const handleLogout = async ({ email }) => {
         try {
             console.log("trigger logout");
             const response = await axiosPrivate.post(LOGOUT_URL, {
@@ -31,6 +33,10 @@ const useLogout = () => {
             queryClient.clear();
 
             navigate("/", { replace: true });
+
+            setType("info");
+            setShowAlert(true);
+            setMessage(`Logout ${email} successfully!`);
         } catch (err) {
             console.log(err);
         }
