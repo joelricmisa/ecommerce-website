c21bd4620e08f852151ada541d4d9f4bcfcad032
joelricmisa <joelriccmisa@gmail.com>
2023-12-12 21:23:01 +0800

    fix(alert): dynamic color bug

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -17,19 +17,25 @@ const Alert = ({ message, type }) => {
         };
     }, [showAlert]);
 
-    const types = {
-        success: "green",
-        delete: "red",
-        info: "blue",
+    const boxVariants = {
+        success: "text-green-900 bg-green-100",
+        delete: "text-red-900 bg-red-100",
+        info: "text-blue-900 bg-blue-100",
     };
 
-    const color = types[type];
+    const iconVariants = {
+        success: "text-green-400",
+        delete: "text-red-400",
+        info: "text-blue-400",
+    };
 
     const containerStyle = `${
         showAlert ? "fixed" : "hidden"
-    } bottom-5 left-10 z-[999]  flex w-auto items-center gap-4 rounded-lg bg-${color}-100 px-5 py-3 text-base text-${color}-900`;
+    } bottom-5 sm:left-10 left-4  z-[999]  flex w-11/12 sm:w-auto items-center justify-center gap-4 rounded-lg px-5  py-3 text-base ${
+        boxVariants[type]
+    }`;
 
-    const iconStyle = `w-1/12 text-xl text-${color}-400`;
+    const iconStyle = `sm:w-1/12 text-xl  ${iconVariants[type]}`;
 
     return (
         <div className={containerStyle}>
@@ -39,7 +45,7 @@ const Alert = ({ message, type }) => {
             {type === "delete" ? <FaTrashAlt className={iconStyle} /> : null}
             {type === "info" ? <FaInfoCircle className={iconStyle} /> : null}
 
-            <p className="w-11/12 whitespace-nowrap pr-1 text-base">
+            <p className=" text-sm sm:w-11/12 sm:whitespace-nowrap sm:pr-5 sm:text-base ">
                 {message}
             </p>
         </div>
