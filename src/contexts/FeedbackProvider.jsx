502c3fd1d622b0c564d9c6c2970238fa27b513e7
joelricmisa <joelriccmisa@gmail.com>
2023-12-14 21:17:57 +0800

    feat(feedback context): add modal and loading overlay

--- src/contexts/FeedbackProvider.jsx
+++ src/contexts/FeedbackProvider.jsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from "react";
 import Alert from "../components/Alert";
+import Modal from "../components/Modal";
+import LoadingOverlay from "../components/LoadingOverlay";
 
 const FeedbackContext = createContext({});
 
@@ -7,24 +9,45 @@ export const FeedbackProvider = ({ children }) => {
     const [showAlert, setShowAlert] = useState(false);
     const [message, setMessage] = useState("");
     const [type, setType] = useState("");
+    const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState("");
+    const [showLoadingOverlay, setShowLoadingOverlay] = useState(false);
+    const [loadingMessage, setLoadingMessage] = useState("");
 
     return (
         <FeedbackContext.Provider
-            value={{ showAlert, setShowAlert, message, setMessage, setType }}
+            value={{
+                showAlert,
+                setShowAlert,
+                message,
+                setMessage,
+                type,
+                setType,
+                showModal,
+                setShowModal,
+                modalMessage,
+                setModalMessage,
+                showLoadingOverlay,
+                setShowLoadingOverlay,
+                loadingMessage,
+                setLoadingMessage,
+            }}
         >
             {children}
 
             {type === "success" ? (
-                <Alert message={message} showAlert={showAlert} type="success" />
+                <Alert message={message} type="success" />
             ) : null}
 
             {type === "delete" ? (
-                <Alert message={message} showAlert={showAlert} type="delete" />
+                <Alert message={message} type="delete" />
             ) : null}
 
-            {type === "info" ? (
-                <Alert message={message} showAlert={showAlert} type="info" />
-            ) : null}
+            {type === "info" ? <Alert message={message} type="info" /> : null}
+
+            <Modal />
+
+            <LoadingOverlay />
         </FeedbackContext.Provider>
     );
 };
