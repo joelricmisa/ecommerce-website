f364ff24714b2c18aab4d26193fabd766d891da7
joelricmisa <joelriccmisa@gmail.com>
2023-12-21 17:14:20 +0800

    feat(require auth): add require auth component

---
+++ src/layouts/RequireAuth.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const RequireAuth = ({ children }) => {
+    const { auth } = useAuth();
+    const location = useLocation();
+
+    if (!auth) {
+        return <Navigate to="/signin" state={{ from: location }} replace />;
+    }
+
+    return children;
+};
+
+export default RequireAuth;
