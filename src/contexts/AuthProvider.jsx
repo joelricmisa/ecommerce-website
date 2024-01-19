beb59135a55ea9c34416ac67f0c23c84bcab120f
joelricmisa <joelriccmisa@gmail.com>
2024-01-19 15:51:32 +0800

    remove console.log in auth

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -4,13 +4,7 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState();
-    useEffect(() => {
-        const interval = setInterval(() => {
-            console.log(auth);
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [auth]);
+   
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
             {children}
