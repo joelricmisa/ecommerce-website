f8d8c27b9d66e660c0216768effc5654db1e8a3e
joelricmisa <joelriccmisa@gmail.com>
2024-01-19 15:42:06 +0800

    test prod

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,10 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState();
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log(auth);
+        }, 1000);
 
+        return () => clearInterval(interval);
+    }, [auth]);
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
             {children}
